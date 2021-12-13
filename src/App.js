17995693084a332149df52e7b6743b9e6aabb7b5
Mukund Raj <mraj@broadinstitute.org>
2021-12-13 11:48:33 -0500

    Minor clean up of App.js. Also config.json edited for testing inph data from local

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Vitessce } from 'vitessce';
+import myViewConfig from './my-view-config.json';
+import 'vitessce/dist/es/production/static/css/index.css';
+import './index.css';
 
 function App() {
+  console.log(myViewConfig);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to ML Portal.</h1>
       </header>
+      <Vitessce
+            config={myViewConfig}
+            height={800}
+            theme="light"
+        />
     </div>
   );
 }
